5b63a0f3bba58c6fd7a3932cddc2ba5260b3cd31
Enzeun <khuynjun123@naver.com>
2026-07-09 14:10:33 +0900

    [feat] 아이템 장비창에 공격력,방어력 표시

--- Item/ItemBase.cs
+++ Item/ItemBase.cs
@@ -141,24 +141,31 @@ public static class ItemData
             if (item is Weapon)
             {
                 Weapon weapon = (Weapon)item;
+
                 if (Player.EquippedWeapon == weapon)
                 {
-                    itemDesc = "[장비됨]";
+                    itemName += " [장비됨]";
                     canUse = false;
                 }         
                 else if (Player.JobName != weapon.CompatibleJob)
                 {
-                    itemDesc = "[장비불가]";
+                    itemName += " [장비불가]";
                     canUse = false;
                 }
+
+                itemDesc += $"공격력 + {weapon.Attack}";
             }
             else if (item is Armor)
             {
-                if (Player.EquippedArmor == item)
+                Armor armor = (Armor)item;
+
+                if (Player.EquippedArmor == armor)
                 {
-                    itemDesc = "[장비됨]";
+                    itemName += " [장비됨]";
                     canUse = false;
                 }
+
+                itemDesc += $"방어력 + {armor.Defence}";
             }
 
             MenuOption menuOption = new MenuOption(count, $"{itemName}", $"{itemDesc}", canUse);
